89adb9e5c9dc2abae62d3f2ab7c2d2de9fb9154a
Yash Shaw <yshaw2002@gmail.com>
2026-02-26 18:13:46 +0530

    added java-springboot app

---
+++ java-springboot-app/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>🚀 Dockerized Spring Boot App</h1>";
+    }
+}
